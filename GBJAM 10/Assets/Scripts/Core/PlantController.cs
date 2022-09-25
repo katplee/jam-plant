b47e61e08b450cc39837133c93daa7d003b20daf
katplee <katpelee@gmail.com>
2022-09-25 20:24:48 +0900

    Control design implementation
    Explored the InputAction.CallbackContext struct for the player control.

--- GBJAM 10/Assets/Scripts/Core/PlantController.cs
+++ GBJAM 10/Assets/Scripts/Core/PlantController.cs
@@ -1,8 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+enum Controls
+{
+    upArrow,
+    downArrow,
+    leftArrow,
+    rightArrow,
+    a,
+    b
+}
 
 public class PlantController : MonoBehaviour
 {
-    private PlantControls controls = new PlantControls();
+    private PlantControls controls = null;
+
+    private void Awake()
+    {
+        controls = new PlantControls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+
+        //move controls
+        controls.Grow.Move.performed += ctx => React(ctx.control.name);
+        controls.Grow.Water.performed += ctx => React(ctx.control.name);
+        controls.Special.Shake.performed += ctx => React(ctx.control.name);
+    }
+
+    void React(string path)
+    {
+        Debug.Log(path);
+    }
+
+    void Special()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+
+
+    }
+
 }
--- GBJAM 10/Assets/Scripts/Core/PlantControls.cs
+++ GBJAM 10/Assets/Scripts/Core/PlantControls.cs
@@ -25,6 +25,14 @@ public class @PlantControls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Water"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""31f08c47-7b00-40e9-b645-b6ab2825e612"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -42,7 +50,7 @@ public class @PlantControls : IInputActionCollection, IDisposable
                 {
                     ""name"": ""negative"",
                     ""id"": ""23e3d516-1442-41ca-95ad-e3d6e14177fd"",
-                    ""path"": ""<Keyboard>/a"",
+                    ""path"": ""<Keyboard>/leftArrow"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -53,7 +61,7 @@ public class @PlantControls : IInputActionCollection, IDisposable
                 {
                     ""name"": ""positive"",
                     ""id"": ""88f0faad-29be-4c76-826f-052137dd7b56"",
-                    ""path"": ""<Keyboard>/d"",
+                    ""path"": ""<Keyboard>/rightArrow"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -75,7 +83,7 @@ public class @PlantControls : IInputActionCollection, IDisposable
                 {
                     ""name"": ""negative"",
                     ""id"": ""f059c2c7-5121-4fdc-b005-855dfcdbad92"",
-                    ""path"": ""<Keyboard>/s"",
+                    ""path"": ""<Keyboard>/downArrow"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -86,13 +94,51 @@ public class @PlantControls : IInputActionCollection, IDisposable
                 {
                     ""name"": ""positive"",
                     ""id"": ""3a0a7144-12ca-4d95-8934-4ad73dc3eb90"",
-                    ""path"": ""<Keyboard>/w"",
+                    ""path"": ""<Keyboard>/upArrow"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ddce2fc4-ebf2-4bd4-bf2a-d66641a0176f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Water"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Special"",
+            ""id"": ""e62a7ca2-6a6f-4eca-baad-e43cf8c48409"",
+            ""actions"": [
+                {
+                    ""name"": ""Shake"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""4e8cc495-daa6-467e-af33-6f4059641328"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""3621795f-a3f5-4d6e-95c0-ae32668b0cfc"",
+                    ""path"": ""<Keyboard>/b"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shake"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -102,6 +148,10 @@ public class @PlantControls : IInputActionCollection, IDisposable
         // Grow
         m_Grow = asset.FindActionMap("Grow", throwIfNotFound: true);
         m_Grow_Move = m_Grow.FindAction("Move", throwIfNotFound: true);
+        m_Grow_Water = m_Grow.FindAction("Water", throwIfNotFound: true);
+        // Special
+        m_Special = asset.FindActionMap("Special", throwIfNotFound: true);
+        m_Special_Shake = m_Special.FindAction("Shake", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -152,11 +202,13 @@ public class @PlantControls : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Grow;
     private IGrowActions m_GrowActionsCallbackInterface;
     private readonly InputAction m_Grow_Move;
+    private readonly InputAction m_Grow_Water;
     public struct GrowActions
     {
         private @PlantControls m_Wrapper;
         public GrowActions(@PlantControls wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Grow_Move;
+        public InputAction @Water => m_Wrapper.m_Grow_Water;
         public InputActionMap Get() { return m_Wrapper.m_Grow; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -169,6 +221,9 @@ public class @PlantControls : IInputActionCollection, IDisposable
                 @Move.started -= m_Wrapper.m_GrowActionsCallbackInterface.OnMove;
                 @Move.performed -= m_Wrapper.m_GrowActionsCallbackInterface.OnMove;
                 @Move.canceled -= m_Wrapper.m_GrowActionsCallbackInterface.OnMove;
+                @Water.started -= m_Wrapper.m_GrowActionsCallbackInterface.OnWater;
+                @Water.performed -= m_Wrapper.m_GrowActionsCallbackInterface.OnWater;
+                @Water.canceled -= m_Wrapper.m_GrowActionsCallbackInterface.OnWater;
             }
             m_Wrapper.m_GrowActionsCallbackInterface = instance;
             if (instance != null)
@@ -176,12 +231,53 @@ public class @PlantControls : IInputActionCollection, IDisposable
                 @Move.started += instance.OnMove;
                 @Move.performed += instance.OnMove;
                 @Move.canceled += instance.OnMove;
+                @Water.started += instance.OnWater;
+                @Water.performed += instance.OnWater;
+                @Water.canceled += instance.OnWater;
             }
         }
     }
     public GrowActions @Grow => new GrowActions(this);
+
+    // Special
+    private readonly InputActionMap m_Special;
+    private ISpecialActions m_SpecialActionsCallbackInterface;
+    private readonly InputAction m_Special_Shake;
+    public struct SpecialActions
+    {
+        private @PlantControls m_Wrapper;
+        public SpecialActions(@PlantControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Shake => m_Wrapper.m_Special_Shake;
+        public InputActionMap Get() { return m_Wrapper.m_Special; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(SpecialActions set) { return set.Get(); }
+        public void SetCallbacks(ISpecialActions instance)
+        {
+            if (m_Wrapper.m_SpecialActionsCallbackInterface != null)
+            {
+                @Shake.started -= m_Wrapper.m_SpecialActionsCallbackInterface.OnShake;
+                @Shake.performed -= m_Wrapper.m_SpecialActionsCallbackInterface.OnShake;
+                @Shake.canceled -= m_Wrapper.m_SpecialActionsCallbackInterface.OnShake;
+            }
+            m_Wrapper.m_SpecialActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Shake.started += instance.OnShake;
+                @Shake.performed += instance.OnShake;
+                @Shake.canceled += instance.OnShake;
+            }
+        }
+    }
+    public SpecialActions @Special => new SpecialActions(this);
     public interface IGrowActions
     {
         void OnMove(InputAction.CallbackContext context);
+        void OnWater(InputAction.CallbackContext context);
+    }
+    public interface ISpecialActions
+    {
+        void OnShake(InputAction.CallbackContext context);
     }
 }
